cea5086ffdfd8f4509ee792a4473c5988f615df1
salsabeel10 <salsabeelibrahim327@gmail.com>
2024-10-28 14:39:46 +0400

    fixed bullet points in result

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -24,39 +24,45 @@ const ContextProvider = (props) => {
   }
 
   const onSent = async (prompt) => {
+    setResultData('')
+    setLoading(true)
+    setShowResult(true)
+    let response
 
-    setResultData("");
-    setLoading(true);
-    setShowResult(true);
-    let response;
-    if (prompt !== undefined){
-      response = await run(prompt);
-      setRecentPrompt(prompt);
-    }else{
-      setPrevPrompts(prev=>[...prev,input]);
-      setRecentPrompt(input);
-      response = await run(input);
+    if (prompt !== undefined) {
+      response = await run(prompt)
+      setRecentPrompt(prompt)
+    } else {
+      setPrevPrompts((prev) => [...prev, input])
+      setRecentPrompt(input)
+      response = await run(input)
     }
-    
 
-    let responseArray = response.split("**")
-    let newResponse ="";
-    for(let i=0; i <responseArray.length; i++){
-      if ( i==0 || i%2 !==1){
-        newResponse +=responseArray[i];
-      }
-      else{
-        newResponse += "<b>"+responseArray[i]+"</b>";
+    // Step 1: Bold formatting for text enclosed in "**"
+    let responseArray = response.split('**')
+    let newResponse = ''
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i == 0 || i % 2 !== 1) {
+        newResponse += responseArray[i]
+      } else {
+        newResponse += '<b>' + responseArray[i] + '</b>'
       }
     }
-    let newResponse2 =newResponse.split("*").join("</br>")
-    let newResponseArray = newResponse2.split(" ");
-    for(let i=0; i<newResponseArray.length;i++){
-      const nextWord =newResponseArray[i];
-      delayPara(i,nextWord+" ")
+
+    // Step 2: Replace single "*" with bullet points
+    let formattedResponse = newResponse.split('*').join('<br>&bull; ')
+
+    // Step 3: Add line breaks for better readability (can be customized further)
+    formattedResponse = formattedResponse.replace(/<\/b><br>/g, '</b><br><br>')
+
+    // Step 4: Split into words and render with delay (or just set innerHTML for immediate display)
+    let newResponseArray = formattedResponse.split(' ')
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i]
+      delayPara(i, nextWord + ' ') // Assuming delayPara handles the delayed rendering
     }
-    setLoading(false);
-    setInput("");
+    setLoading(false)
+    setInput('')
   }
 
 
